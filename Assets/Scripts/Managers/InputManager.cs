caf269f80fe956066e8628c4695769922b06c4c8
Daniel Marcionilo <danielmarcionilo2002@gmail.com>
2025-06-01 19:06:31 -0300

    ao passar o mouse em cima da célula a célula fica com cinza claro indicando que será clicada

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,7 +1,8 @@
 using System;
 using UnityEngine;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : MonoBehaviour
+{
 
     public static InputManager Instance { get; private set; }
 
@@ -13,64 +14,92 @@ public class InputManager : MonoBehaviour {
 
     private Vector3 lastMousePosition;
 
-    private void Awake() {
-        if (Instance != null) {
+    [SerializeField] private Material previewMaterial;
+
+private GamePosition lastHoveredCell;
+private Material lastHoveredOriginalMaterial;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
             Debug.LogError("More than one InputManager instance!");
         }
         Instance = this;
     }
 
-    private void Update() {
-        // Vector3 mousePos = Input.mousePosition;
-        // mousePos.z = mainCamera.nearClipPlane;
-        // Ray ray = mainCamera.ScreenPointToRay(mousePos);
-        // RaycastHit hit;
+private void Update() {
+    Ray ray = GetRaycastHit();
+    if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, gridLayerMask)) {
+        GamePosition cell = hit.collider.GetComponent<GamePosition>();
 
-        // if (Physics.Raycast(ray, out hit, 100, layerMask)) {
-        //     lastMousePosition = hit.point;
-        //     try {
-        //         GamePosition gamePosition = hit.collider.GetComponent<GamePosition>();
-        //         Grid grid = gamePosition.transform.parent.GetComponent<Grid>();
-        //         Vector3Int gridPosition = grid.WorldToCell(hit.point);
-        //         // mouseIndicator.transform.position = grid.GetCellCenterWorld(gridPosition);
-        //     } catch (System.Exception e) {
-        //         // Debug.Log(e.Message);
-        //     }
-        // }
+        if (cell != null && cell != lastHoveredCell) {
+            ClearLastPreview();
+
+            Renderer renderer = cell.GetComponent<Renderer>();
+            lastHoveredOriginalMaterial = renderer.sharedMaterial;
+            renderer.sharedMaterial = previewMaterial;
+
+            lastHoveredCell = cell;
+
+            Debug.Log("Preview sobre: " + cell.name);
+        }
+    } else {
+        ClearLastPreview();
     }
+}
 
-    public Ray GetRaycastHit() {
+
+private void ClearLastPreview() {
+    if (lastHoveredCell != null) {
+        Renderer renderer = lastHoveredCell.GetComponent<Renderer>();
+        renderer.sharedMaterial = lastHoveredOriginalMaterial;
+        lastHoveredCell = null;
+    }
+}
+
+
+    public Ray GetRaycastHit()
+    {
         return mainCamera.ScreenPointToRay(Input.mousePosition);
     }
 
-    public Grid GetGridBeaingIntercepted(out RaycastHit hit) {
+    public Grid GetGridBeaingIntercepted(out RaycastHit hit)
+    {
         Ray ray = GetRaycastHit();
         bool didRaycastHitSomething = Physics.Raycast(ray, out hit, Mathf.Infinity, gridLayerMask);
-        if (didRaycastHitSomething) {
+        if (didRaycastHitSomething)
+        {
             return hit.collider.GetComponent<Grid>();
         }
         return null;
     }
 
-    public IBoat GetBoatBeaingIntercepted(out RaycastHit hit) {
+    public IBoat GetBoatBeaingIntercepted(out RaycastHit hit)
+    {
         Ray ray = GetRaycastHit();
         bool didRaycastHitSomething = Physics.Raycast(ray, out hit, Mathf.Infinity, boatLayerMask);
-        if (didRaycastHitSomething) {
+        if (didRaycastHitSomething)
+        {
             return hit.collider.GetComponent(typeof(IBoat)) as IBoat;
         }
         return null;
     }
 
-    public GameObject GetGameObjectBeaingIntercepted(out RaycastHit hit) {
+    public GameObject GetGameObjectBeaingIntercepted(out RaycastHit hit)
+    {
         Ray ray = GetRaycastHit();
         bool didRaycastHitSomething = Physics.Raycast(ray, out hit, Mathf.Infinity, defaultLayerMask);
-        if (didRaycastHitSomething) {
+        if (didRaycastHitSomething)
+        {
             return hit.collider.gameObject;
         }
         return null;
     }
 
-    public bool IsRayHittingBoat(out RaycastHit hit) {
+    public bool IsRayHittingBoat(out RaycastHit hit)
+    {
         Ray ray = GetRaycastHit();
         return Physics.Raycast(ray, out hit, Mathf.Infinity, boatLayerMask);
     }
